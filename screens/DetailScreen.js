7cfd090a703db2f1148a4b792c8e29fd60002a4f
Pete Latham <github@petelatham.com>
2019-03-15 14:31:46 +0000

    Extras added
    Articles retrieved by API
    Publishing info added
    Read more links to original article in browser

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,30 +1,54 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Image, ScrollView, Text } from 'react-native';
+import { WebBrowser } from 'expo';
+
+import moment from 'moment';
 
 class DetailScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePress = this.handlePress.bind(this);
+  }
+
   static navigationOptions = () => ({
-    // title: navigation.getParam('contact').name,
     title: 'BBC News',
     headerStyle: {
       backgroundColor: '#d35400',
     },
     headerTintColor: '#ffffff',
+    result: '',
   });
 
+  handlePress = async () => {
+    let { url } = this.props.navigation.getParam('news');
+    let result = await WebBrowser.openBrowserAsync(url);
+    this.setState({ result });
+  };
+
   render() {
-    let { title, description, urlToImage } = this.props.navigation.getParam(
-      'news'
-    );
+    let {
+      title,
+      description,
+      urlToImage,
+      publishedAt,
+    } = this.props.navigation.getParam('news');
 
     return (
       <View style={styles.container}>
         <Image style={styles.image} source={{ uri: urlToImage }} />
-        <ScrollView>
+        <ScrollView style={styles.textBox}>
           <Text style={styles.articleTitle}>{title}</Text>
+          <Text style={styles.publication}>
+            Published: {moment(publishedAt).fromNow()}
+          </Text>
           <View style={styles.hrCarrier}>
             <View style={styles.hr} />
           </View>
           <Text style={styles.articleDetail}>{description}</Text>
+          <Text style={styles.more} onPress={this.handlePress}>
+            Read more ...
+          </Text>
         </ScrollView>
       </View>
     );
@@ -45,20 +69,32 @@ const styles = StyleSheet.create({
     height: 200,
     marginBottom: 10,
   },
+  textBox: {
+    width: '90%',
+  },
   articleTitle: {
     fontSize: 24,
+    marginBottom: 4,
+  },
+  publication: {
+    fontSize: 16,
+    color: '#777',
     marginBottom: 10,
   },
   articleDetail: {
     fontSize: 18,
     marginVertical: 5,
   },
+  more: {
+    fontSize: 16,
+    color: '#d35400',
+  },
   hrCarrier: {
     alignItems: 'center',
   },
   hr: {
     height: 1,
-    borderColor: '#ccc',
+    borderColor: '#777',
     borderStyle: 'solid',
     borderWidth: 1,
     marginBottom: 7,
